1644a5ad41a81ccb1b14382c6c0bcf2fc8882f80
bryce Kratzer <brycekratzer@u.boisestate.edu>
2024-10-24 17:55:08 -0600

    Fixing Clock

--- clock_div.v
+++ clock_div.v
@@ -1,35 +1,56 @@
-module div(
-    input clock,
+module dff(
     input reset,
-    output reg div_clock
+    input clock,
+    input D,
+    output reg Q,
+    output NotQ
 );
-    reg intreset;
-    wire [16:0] intcount;
 
-    // Instantiate the counter
-    counter #(.WIDTH(17)) count(
-        .clock(clock),
-        .reset(intreset),
-        .count(intcount)
-    );
-
-    // Synchronous logic block
-    always @(posedge clock or posedge reset) begin
+    always @(posedge reset, posedge clock) begin
         if (reset) begin
-            // If we get reset, pass it through
-            // to the counter and reset output clock
-            intreset <= 1;
-            div_clock <= 0;
-        end 
-        else if (intcount == 100000) begin
-            // Reset the counter and toggle output clock
-            intreset <= 1;
-            div_clock <= ~div_clock;
-        end 
-        else begin
-            // Otherwise, just continue counting
-            intreset <= 0;
+            Q <= 0;
+        end else begin
+            Q <= D;
         end
     end
+    assign NotQ = ~Q;
+endmodule
+
+module clock_div
+#(
+    parameter DIVIDE_BY = 17 // Or 100,000 for counter implementation
+)
+(
+    input clock,
+    input reset,
+    output reg div_clock // Slowed down clock
+);
+wire [DIVIDE_BY:0]clock_out;
+wire [DIVIDE_BY:1]Din;
+
+
+
+dff ddflip(
+    .reset(reset),
+    .clock(clock),
+    .D(Din[0]),
+    .Q(clock_out[0])
+);
+// synchronous logic block, sensitive to rising edge of clock or reset
+
+
+assign clock_out= ~Din;
+
+genvar i;
+generate
+    for(i = 1; i < DIVIDE_BY; i = i + 1)begin
+        dff ddflip(
+            .reset(reset),
+            .clock(clock_out),
+            .D(Din[i-1:0]),
+            .Q(clock_out[i:0])
+        );
+    end
+endgenerate
 
 endmodule
